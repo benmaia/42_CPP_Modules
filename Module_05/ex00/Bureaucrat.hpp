94ec4e7881a6b51794f99275e9861187071baefc
benmaia <godlinhomaia@gmail.com>
2022-08-30 00:49:03 +0100

    ex00

--- Module_05/ex00/Bureaucrat.hpp
+++ Module_05/ex00/Bureaucrat.hpp
@@ -1,7 +1,9 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+#include <exception>
 #include <iostream>
+#include <ostream>
 #include <string.h>
 
 class Bureaucrat
@@ -11,8 +13,33 @@ class Bureaucrat
 		int					_grade;
 
 	public:
+		std::string	getName(void);
+		int	getGrade(void);
+		void	incrementGrade(void);
+		void	decrementGrade(void);
 		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &original);
 		~Bureaucrat();
+		Bureaucrat &operator=(const Bureaucrat &original);
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return "GradeTooHighExecption";
+				}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return "GradeTooLowExecption";
+				}
+		};
 };
+
+std::ostream &operator<<(std::ostream &stream, Bureaucrat &Bureaucrat);
 
 #endif
---
+++ Module_05/ex00/main.cpp
@@ -0,0 +1,105 @@
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+    Bureaucrat bur("Amelia", 150);
+    std::cout << bur.getName() << std::endl;
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "--------------------" << std::endl;
+
+    Bureaucrat other("Maria", 1);
+    std::cout << other.getName() << std::endl;
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "-----TO LOW---------------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Olga", 151);
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "------TO HIGH--------------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Graça", 0);
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "--------------------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Linda", 5);
+        std::cout << other_bur.getName() << std::endl;
+        std::cout << other_bur.getGrade() << std::endl;
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "------DECREMENT--------------" << std::endl;
+
+    try 
+    {
+        bur.decrementGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "-------INCREMENT-------------" << std::endl;
+
+    try 
+    {
+        bur.incrementGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "--------INCREMENET------------" << std::endl;
+
+    try 
+    {
+        other.incrementGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "-------DECREMENT-------------" << std::endl;
+
+    try 
+    {
+        other.decrementGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "--------------------" << std::endl;
+
+    std::cout << bur << std::endl;
+    
+    std::cout << "--------------------" << std::endl;
+    
+    return (0);
+}
